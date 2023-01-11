2493befa7c39c89fab003e972699300ab9adde0f
Ivanishkina-li <ivanishkina2014@mail.ru>
2023-01-11 18:37:50 +0300

    t

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,28 @@ const parserMiddleWare = express.json()
 
 app.use(parserMiddleWare)
 
+const dateNow = new Date();
+
+
+let DayPlusOne = (date:any)=>{
+  date.setDate(date.getDate() + 1)
+  return date.toISOString();
+} 
+
+//фун-ция проверяет указано хотя бы одно разрешение из массива resolutions
+let Isresolution = (res:string) =>{
+  let resolutions = [ 'P144','P240', "P360", "P480", "P720", "P1080", "P1440", "P2160"  ];
+ if(resolutions.includes(res)){
+ return true;
+ }
+ else{
+  return false
+ }
+}
+
+
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('this is video api')
 })
@@ -18,8 +40,8 @@ let videosDB: any = [
     author: "IT-kamasutra",
     canBeDownloaded: true,
     minAgeRestriction: null,
-    createdAt: new Date().toISOString(),
-    publicationDate: new Date().toISOString(),
+    createdAt: dateNow.toISOString(),
+    publicationDate: DayPlusOne(dateNow),
     availableResolutions: ["P720", "P1080", "P1440", "P2160"],
   },
   {
@@ -28,8 +50,8 @@ let videosDB: any = [
     author: "мы обречены",
     canBeDownloaded: true,
     minAgeRestriction: null,
-    createdAt: new Date().toISOString(),
-    publicationDate: new Date().toISOString(),
+    createdAt: dateNow.toISOString(),
+    publicationDate: DayPlusOne(dateNow),
     availableResolutions: ["P720", "P1080", "P1440", "P2160"],
   },
 ];
@@ -41,8 +63,8 @@ app.post("/videos", (req: Request, res: Response) => {
   let title = req.body.title;
   let author = req.body.author;
   let availableResolutions = req.body.availableResolutions;
-  const dateNow = new Date();
-  const dateNowPlusOneDay = +dateNow + 1000 * 60 * 60 * 24;
+
+  // const dateNowPlusOneDay = +dateNow + 1000 * 60 * 60 * 24;
 
   if (
     !title ||
@@ -75,7 +97,7 @@ app.post("/videos", (req: Request, res: Response) => {
       return;
     }
     if (
-      availableResolutions.length < 1 ||
+      Isresolution(availableResolutions)==false ||
       typeof availableResolutions !== "string"
     ) {
       res.status(400).send({
@@ -93,7 +115,7 @@ app.post("/videos", (req: Request, res: Response) => {
         canBeDownloaded: req.body.canBeDownloaded || false,
         minAgeRestriction: req.body.minAgeRestriction || null,
         createdAt: dateNow.toISOString(),
-        publicationDate: dateNow.toISOString(),
+        publicationDate: DayPlusOne(dateNow) ,
         availableResolutions: req.body.availableResolutions,
       };
       videosDB.push(newVideo);
@@ -103,18 +125,48 @@ app.post("/videos", (req: Request, res: Response) => {
 });
 
 app.put("/videos/Videoid", (req: Request, res: Response) => {
+  let author = req.body.author;
   let title = req.body.title;
-  if (!title || typeof title !== "string" || title.trim()) {
+  let availableResolutions = req.body.availableResolutions;
+  if (!title || typeof title !== "string" || title.trim() ||  title.length > 40) {
     res.status(400).send({
       errorsMessages: [
         {
-          message: "string",
+          message: "invalid string",
           field: "string",
         },
       ],
     });
     return;
   }
+  if (
+    !author ||
+    typeof author !== "string" ||
+    !title.trim() ||
+    title.length > 20
+  ) {
+    res.status(400).send({
+      errorsMessages: [
+        {
+          message: "author is incorrect",
+          field: "author",
+        },
+      ],
+    });
+    return;
+  }
+  if (
+      Isresolution(availableResolutions)==false ||
+      typeof availableResolutions !== "string"
+    ) {
+      res.status(400).send({
+        errorsMessages: [
+          {
+            message: "availableResolutions is incorrect",
+            field: "availableResolutions",
+          },
+        ],
+      })};
   let id = +req.params.Videoid;
   const video = videosDB.find((v: any) => v.id === id);
 
@@ -133,6 +185,17 @@ app.get("/videos/:Videoid", (req: Request, res: Response) => {
   } else {
     res.send(404);
   }
+});
+
+app.delete("/videos/:Videoid", (req: Request, res: Response) => {
+  for (let i = 0; i < videosDB.length; i++) {
+    if (videosDB[i].id === +req.params.Videoid) {
+      videosDB.splice(i, 1);
+      res.send(204);
+      return;
+    }
+  }
+  res.send(404);
 });
 
 app.listen(port, () => {
